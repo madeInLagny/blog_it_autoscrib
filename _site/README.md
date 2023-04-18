990ac41543afe4bc8463a197c7ecc594285f39da
Jocelyn <dev@madeinlagny.com>
2023-04-18 10:09:40 +0200

    _site removed from git ignore

---
+++ _site/README.md
@@ -0,0 +1,11 @@
+<!--
+run the blog with 'bundle exec jekyll serve'
+ -->
+
+<!--
+customize @ https://mmistakes.github.io/minimal-mistakes/docs/configuration/
+ -->
+
+# autoScrib Blog in Italian
+
+Live on [autoscrib blog](https://it.blog.autoscrib.com)
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "7 modi per rendere le riunioni e i corsi di formazione più produttivi",
+        "excerpt":"Foto di Evangeline Shaw su Unsplash &lt;/a&gt; la tecnologia di riconoscimento vocale sviluppata da autoScrib ti aiuta ad aumentare la tua produttività durante le tue riunioni, la formazione faccia a faccia o online, o anche i tuoi corsi o tutorial. Consente di prendere appunti in modo completo e istantaneo. Sapevi...","categories": [],
+        "tags": ["Produttività","Formazione"],
+        "url": "/7-modi-per-rendere-le-riunioni-e-i-corsi-di-formazione-pi%C3%B9-produttivi/",
+        "teaser": "/assets/images/posts/20221114-hero.jpg"
+      },{
+        "title": "Riconoscimento vocale: AI per le persone con difficoltà di apprendimento",
+        "excerpt":"Foto di Volodymyr Hryshchenko su Unsplash La tecnologia di trascrizione orale derivata dall’intelligenza artificiale (AI), chiamata anche riconoscimento vocale o dettatura assistita, consente di scrivere parole pronunciandole ad alta voce. È particolarmente utile per le persone con disgrafia o dislessia e altre difficoltà di apprendimento e di pensiero che influiscono...","categories": [],
+        "tags": ["dyslexia","dysgraphia"],
+        "url": "/Riconoscimento-vocale-AI-per-le-persone-con-difficolt%C3%A0-di-apprendimento/",
+        "teaser": "/assets/images/posts/20230328-hero.webp"
+      },{
+        "title": "Lo stato del riconoscimento vocale",
+        "excerpt":"Foto di Bruce Mars su Unsplash La trascrizione dal suono al testo, aka sintesi vocale o riconoscimento vocale, è il processo di conversione delle parole pronunciate in testo scritto, utilizzato principalmente per prendere appunti automaticamente. È una tecnologia che è diventata sempre più popolare negli ultimi anni grazie alle sue...","categories": [],
+        "tags": ["technologia","trascrizione"],
+        "url": "/Lo-stato-del-riconoscimento-vocale/",
+        "teaser": "/assets/images/posts/20230403-hero.jpg"
+      },{
+        "title": "Ecco perché dovresti usare l'intelligenza artificiale nelle tue riunioni.",
+        "excerpt":"Foto id Jason Goodman su Unsplash Il 2023 ha già visto molte nuove innovazioni nell’IA. È più chiaro che mai che l’adozione di nuovi strumenti per riunioni IA può migliorare la collaborazione e la produttività in tutta l’organizzazione. Ecco alcuni motivi per cui dovresti prendere in considerazione l’integrazione dell’IA nella...","categories": [],
+        "tags": ["Produttività","Incontro","Meeting","AI"],
+        "url": "/Ecco-perch%C3%A9-dovresti-usare-l'intelligenza-artificiale-nelle-tue-riunioni/",
+        "teaser": "/assets/images/posts/20230409-hero.jpg"
+      },{
+        "title": "Ecco cosa rende un buon assistente vocale",
+        "excerpt":"Foto di Jason Rosewell su Unsplash La facilità d’uso del riconoscimento vocale non dovrebbe mettere in ombra le tecnologie avanzate che utilizza. Algoritmo di riconoscimento vocale, capacità di rilevare accenti e dialetto, gestione del ‘rumore di fondo’, velocità di trascrizione del dettato… una soluzione di trascrizione vocale ad alte prestazioni...","categories": [],
+        "tags": ["Technologia","Natural Language Processing"],
+        "url": "/Ecco-cosa-rende-un-buon-assistente-vocale/",
+        "teaser": "/assets/images/posts/20230418-hero.jpg"
+      }]
